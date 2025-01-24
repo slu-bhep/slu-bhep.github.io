39eb4232782fdd8f196fe33952ab24739c2bf582
slu-bhep <slu@birchhillequity.com>
2025-01-24 15:54:29 -0500

    remove extra files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import {Header} from "./components/Header"
+import {Clock} from "./components/Clock"
+import {Footer} from "./components/Footer"
+import styled from 'styled-components'
+
+// custom styling using styled-components!
+const AppContainer = styled.div`
+  margin: 40vh 30vw;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <Header/>
+      <Clock/>
+      <Footer/>
+    </AppContainer>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'; // Update the import for React 18
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Get the root DOM node
+const rootElement = document.getElementById('root');
+
+// Create a root and render the App
+const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
